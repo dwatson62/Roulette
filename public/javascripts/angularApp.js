8a260e387b5e4c3e40e76913b836ee11c0bf8408
Daryl Watson <darylwtsn@gmail.com>
2015-07-20 23:08:44 +0100

    Displays starting balance

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -2,4 +2,9 @@ var roulette = angular.module('Roulette', []);
 
 roulette.controller('RouletteController', [function() {
   var self = this;
+  var player = new Player();
+  var wheel = new Wheel();
+
+  self.playerBalance = player.balance;
+
 }]);
--- spec/e2e/rouletteFeatureSpec.js
+++ spec/e2e/rouletteFeatureSpec.js
@@ -1,10 +1,17 @@
 describe('Roulette', function () {
+
+  var playerBalance = element(by.id('player-balance'));
+
   beforeEach(function (){
     browser.get('http://localhost:3000')
   });
 
   it('Has a title', function () {
     expect(browser.getTitle()).toEqual('Roulette');
+  });
+
+  it('Displays player balance', function() {
+    expect(playerBalance.getText()).toEqual('£100');
   });
 
 });
