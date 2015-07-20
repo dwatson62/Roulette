cd774f141068ebc3db798b4faa0ef8013f2c1763
Daryl Watson <darylwtsn@gmail.com>
2015-07-21 00:28:31 +0100

    Can bet on colour. Past results are shown

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -5,11 +5,41 @@ roulette.controller('RouletteController', [function() {
   var player = new Player();
   var wheel = new Wheel();
 
+  self.spinResult = [];
   self.playerBalance = player.balance;
+  self.pastSpins = [];
 
   self.numberBet = function(number) {
-    console.log(number);
-    self.number = number;
+    self.bet = number;
+  };
+
+  self.colourBet = function(colour) {
+    console.log(colour);
+    self.bet = colour;
+  };
+
+  self.spin = function() {
+    wheel.spin();
+    self.spinResult[0] = wheel.number;
+    self.spinResult[1] = wheel.colour;
+    if (self.bet == null) { return; }
+    self.updateBalance();
+    self.spinHistory();
+  };
+
+  self.spinHistory = function() {
+    self.pastSpins.push( { 'number': self.spinResult[0], 'colour': self.spinResult[1] } );
+  };
+
+  self.updateBalance = function() {
+    if (self.bet == wheel.number) {
+      player.balance += (10 * 35);
+    } else if (self.bet == wheel.colour) {
+      player.balance += (10 * 2);
+    } else {
+      player.balance -= 10;
+    }
+    self.playerBalance = player.balance;
   };
 
 }]);
--- spec/e2e/rouletteFeatureSpec.js
+++ spec/e2e/rouletteFeatureSpec.js
@@ -1,10 +1,13 @@
 var playerBalance = element(by.id('player-balance'));
-var playerBet = element(by.id('player-bet'))
+var playerBet = element(by.id('player-bet'));
 var numberBtn = element(by.className('number-btn'));
+var spinBtn = element(by.id('spin-btn'));
+var spinResult = element(by.id('spin-result'));
+var redBtn = element(by.id('red-btn'));
 
 beforeEach(function (){
-    browser.get('http://localhost:3000')
-  });
+  browser.get('http://localhost:3000');
+});
 
 describe('Roulette table', function () {
 
@@ -12,7 +15,7 @@ describe('Roulette table', function () {
     expect(browser.getTitle()).toEqual('Roulette');
   });
 
-  it('Displays player balance', function() {
+  xit('Displays player balance', function() {
     expect(playerBalance.getText()).toEqual('£100');
   });
 
@@ -24,5 +27,21 @@ describe('Betting', function () {
   it('Can bet on a number', function() {
     numberBtn.click();
     expect(playerBet.getText()).toEqual('You bet on 0');
+  });
+
+  it('Can bet on a colour', function() {
+    redBtn.click();
+    expect(playerBet.getText()).toEqual('You bet on Red');
+  });
+
+  it('Can spin the wheel and see the result', function() {
+    spinBtn.click();
+    expect(spinResult.getText()).toContain('Landed on');
+  });
+
+  xit('Can bet on 0 and win if the wheel spins 0', function() {
+    numberBtn.click();
+    spinBtn.click();
+    expect(playerBalance.getText()).toChange();
   });
 });
