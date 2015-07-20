ecab321b86288798f8d589dc8b87eeaa200eac92
Daryl Watson <darylwtsn@gmail.com>
2015-07-20 16:57:54 +0100

    Knows when number is red

--- public/javascripts/wheel.js
+++ public/javascripts/wheel.js
@@ -1,6 +1,22 @@
 function Wheel() {
+	this.colour;
+	this.number;
 };
 
 Wheel.prototype.spin = function() {
-	return Math.floor((Math.random() * 36) + 1);	
+	this.number = Math.floor((Math.random() * 36) + 1);	
+	this.setColour();
+};
+
+Wheel.prototype.setColour = function() {
+	var reds = [1, 3, 5, 7, 9, 12, 13, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+	for (i = 0; i < reds.length; i ++ ) {	
+		if (i == this.number) {
+			this.colour = 'Red'
+			return;
+		}
+		else {
+			this.colour = 'Black'
+		}
+	}
 };
--- spec/wheel.js
+++ spec/wheel.js
@@ -1,7 +1,22 @@
+beforeEach(function() {
+	wheel = new Wheel;
+});
 describe('Returns a number', function() {
 	it('between 0 and 36', function() {
 		wheel = new Wheel;
-		var result = wheel.spin();
-		expect(result >= 0 && result < 37).toBeTruthy();
+		wheel.spin();
+		expect(wheel.number >= 0 && wheel.number < 37).toBeTruthy();
+	});
+});
+
+// reds 1 3 5 7 9 12 13 16 18 19 21 23 25 27 30 32 34 36
+
+describe('Return the correct colour', function() {
+	it('when given 1', function() {
+		spyOn(Math, 'random').and.returnValue(0.01);
+		wheel.spin();
+		console.log(wheel.number)
+		console.log(wheel.colour)
+		expect(wheel.colour).toEqual('Red');
 	});
 });
