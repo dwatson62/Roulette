537cf4b9dd6686607c295ed821bee647e6dc3bbc
Daryl Watson <darylwtsn@gmail.com>
2015-07-21 11:59:53 +0100

    Added test for past spins

--- spec/e2e/rouletteFeatureSpec.js
+++ spec/e2e/rouletteFeatureSpec.js
@@ -1,9 +1,11 @@
+var numberBtn = element(by.className('number-btn'));
+var oddBtn = element(by.id('odd-btn'));
+var pastSpins = element.all(by.repeater('pastSpins in rltCtrl.pastSpins'));
 var playerBalance = element(by.id('player-balance'));
 var playerBet = element(by.id('player-bet'));
-var numberBtn = element(by.className('number-btn'));
+var redBtn = element(by.id('red-btn'));
 var spinBtn = element(by.id('spin-btn'));
 var spinResult = element(by.id('spin-result'));
-var redBtn = element(by.id('red-btn'));
 
 beforeEach(function (){
   browser.get('http://localhost:3000');
@@ -15,10 +17,16 @@ describe('Roulette table', function () {
     expect(browser.getTitle()).toEqual('Roulette');
   });
 
-  xit('Displays player balance', function() {
+  it('Displays player balance', function() {
     expect(playerBalance.getText()).toEqual('£100');
   });
 
+  it('Dispays previous spin history', function() {
+    spinBtn.click();
+    pastSpins.then(function(result) {
+      expect(result.length).toBeGreaterThan(0);
+    });
+  });
 
 });
 
@@ -32,6 +40,11 @@ describe('Betting', function () {
   it('Can bet on a colour', function() {
     redBtn.click();
     expect(playerBet.getText()).toEqual('You bet on Red');
+  });
+
+  it('Can bet on odd/even', function() {
+    oddBtn.click();
+    expect(playerBet.getText()).toEqual('You bet on Odd');
   });
 
   it('Can spin the wheel and see the result', function() {
