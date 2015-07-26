c7b69a996fa7019e13d614f301931071fd6ad061
Daryl Watson <darylwtsn@gmail.com>
2015-07-26 14:14:48 +0100

    Refactored player.js a little

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -11,40 +11,40 @@ Player.prototype.resetWinnings = function() {
 	this.winnings = 0;
 };
 
+Player.prototype.collectWinnings = function(amount, factor) {
+	this.balance += (amount * (factor + 1));
+	this.winnings += (amount * factor);
+};
+
 Player.prototype.colourBetCheck = function(amount, option, wheel) {
-	if (wheel.colour === option) {
-		this.balance += (amount * 3);
-		this.winnings += (amount * 2);
+  if (wheel.colour === option) {
+    this.collectWinnings(amount, 2)
 	}
 };
 
 Player.prototype.numberBetCheck = function(amount, option, wheel) {
 	if (wheel.number === option) {
-		this.balance += (amount * 36);
-		this.winnings += (amount * 35);
+    this.collectWinnings(amount, 35)
 	}
 };
 
 Player.prototype.oddOrEvenBetCheck = function(amount, option, wheel) {
 	if (wheel.oddOrEven === option) {
-		this.balance += (amount * 3);
-		this.winnings += (amount * 2);
+    this.collectWinnings(amount, 2)
 	}
 };
 
 Player.prototype.columnBetCheck = function(amount, option, wheel) {
 	var columnNumber = parseInt(option.split('').pop());
     if (wheel.columnNumber === columnNumber) {
-      this.balance += (amount * 3);
-      this.winnings += (amount * 2);
+      this.collectWinnings(amount, 2)
     }
 };
 
 Player.prototype.dozenBetCheck = function(amount, option, wheel) {
   var dozenNumber = parseInt(option.split('').pop());
   if (wheel.dozenNumber === dozenNumber) {
-    this.balance += (amount * 3);
-    this.winnings += (amount * 2);
+    this.collectWinnings(amount, 2)
    }
 };
 
@@ -52,7 +52,6 @@ Player.prototype.highLowBetCheck = function(amount, option, wheel) {
   var first = parseInt(option.split('').splice(0, 2).join(''));
   var second = parseInt(option.split('').splice(-2).join(''));
   if (first <= wheel.number && wheel.number <= second) {
-    this.balance += (amount * 3);
-    this.winnings += (amount * 2);
+    this.collectWinnings(amount, 2)
   }
 };
