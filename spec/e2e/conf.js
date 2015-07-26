00d30e6d306a72c439afae0d14380810f9d6c824
Daryl Watson <darylwtsn@gmail.com>
2015-07-26 18:31:07 +0100

    Fixed typo in protractor conf.js

--- spec/e2e/conf.js
+++ spec/e2e/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['RouletteFeatureSpec.js'],
+  specs: ['rouletteFeatureSpec.js'],
   capabilites: {
     browserName: 'chrome'
   }
